07e3b6aec7f08033bd5762ec49a0be227bcbf88d
okabe.isao <okab130@vultr.guest>
2021-03-22 12:32:54 +0000

    4nd 2021/3/22 upd rest api

--- ModelApp/views.py
+++ ModelApp/views.py
@@ -1,30 +1,30 @@
 
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework import generics
 
 from .models import Person
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from .serializers import PersonSerializer,UserSerializer
-
+from .ownpermissions import ProfilePermission
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
+    permission_classes = (ProfilePermission,)
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     authentication_classes=(TokenAuthentication,)
     permission_classes=(IsAuthenticated,)
     
-    def get_obeject(self):
+    def get_object(self):
         return self.request.user
 
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
-    authentication_class = (TokenAuthentication,)
+    authentication_classes = (TokenAuthentication,)
     permission_classes =(IsAuthenticated,)
